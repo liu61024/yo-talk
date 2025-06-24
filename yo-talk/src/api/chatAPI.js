8cfeb6a9532607b4dca3ccbcd31f788acd17890a
yilingliu <liu61024@gmail.com>
2025-06-24 17:44:34 +0800

    render node

--- yo-talk/src/api/chatAPI.js
+++ yo-talk/src/api/chatAPI.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_URL = "https://yo-talk-server.onrender.com/api/chat";
+
 export const sendMessageToAI = async (message) => {
   try {
-    const response = await axios.post("http://localhost:5000/api/chat", {
+    const response = await axios.post("API_URL", {
       //發送請求到後端
       message,
     });
